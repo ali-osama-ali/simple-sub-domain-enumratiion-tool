4f8004d5b36b307a6e6d99211d30ba803fffc8f2
ali-osama-ali <85011048+ali-osama-ali@users.noreply.github.com>
2022-05-28 08:38:06 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+import requests
+
+domain = input("Please , enter the domain you want to enumrate : ")
+
+wordlist = open("wordlist2.txt", "r")
+
+sub_domains = wordlist.read()
+sub_domains = list(sub_domains.split("\n"))
+
+# print(len(sub_domains))
+
+i = 0
+
+while i < len(sub_domains):
+    sub_domain = sub_domains[i]
+    sub_domain = f"http://{sub_domain}.{domain}"
+    try:
+        requests.get(sub_domain)
+    except requests.ConnectionError:
+        pass
+    else:
+        print(f"Connected to : {sub_domain}")
+    i += 1
+wordlist.close()
